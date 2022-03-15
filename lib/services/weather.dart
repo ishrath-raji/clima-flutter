499ce365174e417311d25607de22a5f477084168
ishrath-raji <ishrathraji8@gmail.com>
2022-03-15 13:24:28 +0530

    first commit in clima

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,31 @@
+import 'location.dart';
+import 'networking.dart';
+
+const apiKey = '54b4d02e227e62d4d7e70c5786950733';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  Future getLocationWeather() async {
+    Location location = new Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
